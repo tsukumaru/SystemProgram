40112b362e161d6a55bd691b477f772ff9472dee
tsukumaru <came.and.rabbit@gmail.com>
2014-07-24 19:22:16 +0900

    一応動くことは動く
    タイムアウトのときを戻り値で返せない…

--- kadai5/q505/mygetchar.c
+++ kadai5/q505/mygetchar.c
@@ -3,43 +3,57 @@
 #include <signal.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/time.h>
 
+#define TIMEOUT -2
+
+int timeOutFlag = 0;
 volatile sig_atomic_t alrm_count;
+
+void print_resut(int res)
+{
+	if (res == -1) 
+		printf("input is \"EOF\"\n");
+	else if (res == -2) 
+		printf("timeout\n");
+	else if (res == -3) 
+		printf("other\n");
+	else 
+		printf("input is \'%c\'\n", res);
+}
+
+int get_resut(int c)
+{
+	if (c == EOF)
+		return -1;
+	else if (c == TIMEOUT)
+		return -2;
+	else if (c >= 0)
+		return c;
+	else 
+		return -3;
+}
 
 void alrm()
 {
-	alrm_count--;
-}
-
-int mygetchar()
-{
-	int c, endval;
-	
-	while (alrm_count > 0) { 
-//		pause();
-		signal(SIGINT, &alrm);
-		c = getchar();
-		printf("%d: %ld\n", alrm_count, time(NULL));
-		
-		if (c > 0) return c;
-		else if (c == EOF) return -1; 
+//	if (alrm_count > 0) 
+		alrm_count--;
+//	printf("alrm_count is %d\n", alrm_count);
+	 if (alrm_count == 0) {
+		timeOutFlag = 1;
+		print_resut(TIMEOUT);
+		exit(1);
 	}
-	
-	return -2;
 }
 
-int main(int argc, char *argv[])
+int mygetchar(int sec)
 {
-	int c;
-	char *e;
+	int c = -3;
 	struct sigaction sa_alarm;
 	struct itimerval itimer;
 	
-	alrm_count = strtol(argv[1], &e, 10);
-	if (*e != '\0') {
-		perror("strtol");
-	}
+	alrm_count = sec;
 
 	memset(&sa_alarm, 0, sizeof(sa_alarm));
 	sa_alarm.sa_handler = alrm;
@@ -49,7 +63,7 @@ int main(int argc, char *argv[])
 		perror("sigaction");
 		exit(1);
 	}
-	
+
 	itimer.it_value.tv_sec = itimer.it_interval.tv_sec = 1;
 	itimer.it_value.tv_usec = itimer.it_interval.tv_usec = 0;
 
@@ -57,8 +71,8 @@ int main(int argc, char *argv[])
 		perror("setitimer");
 		exit(1);
 	}
-
-	c = mygetchar();
+//	printf("%d", alrm_count);
+	c = getchar();
 
 	itimer.it_value.tv_sec = itimer.it_interval.tv_sec = 0;
 	itimer.it_value.tv_usec = itimer.it_interval.tv_usec = 0;
@@ -67,6 +81,24 @@ int main(int argc, char *argv[])
 		perror("setitimer");
 		exit(1);
 	}
+	
+	return get_resut(c);
+}
 
+int main(int argc, char *argv[])
+{
+	int c, sec;
+	char *e;
+	
+	sec = strtol(argv[1], &e, 10);
+	if (*e != '\0') {
+		perror("strtol");
+	}
+
+	c = mygetchar(sec);
+	
+//	printf("input is \'%c\'\n", c);
+	print_resut(c);
+	
 	return 0;
 }
